509d0336ad75036e61b88c2a9eb806e3efc092d8
Jinyoung Kim <jinyoung777@gmail.com>
2020-06-09 10:55:32 +1200

    polish business detail page

--- src/screens/ResultShowScreen.js
+++ src/screens/ResultShowScreen.js
@@ -25,7 +25,16 @@ const ResultShowScreen = ({navigation}) => {
     }
 
     return <View >
-        <Text >{result.name}</Text>
+        <Text style={styles.titleStyle}>{result.name}</Text>
+        <Text style={styles.contents}>{result.display_phone}</Text>
+        <Text style={styles.contents}>
+            {result.location.address1 
+                + (result.location.address2 ? ' ' + result.location.address2 : '')
+                + (result.location.address3 ? ' ' + result.location.address3 : '')
+                + (result.location.city ? ' ' + result.location.city : '')
+            }
+        </Text>
+        <Text style={styles.contents}>{result.rating} Stars, {result.review_count} Reviews</Text>
         <FlatList
             data={result.photos}
             keyExtractor={(photo)=>photo}
@@ -37,11 +46,23 @@ const ResultShowScreen = ({navigation}) => {
 }
 
 const styles = StyleSheet.create({
+    titleStyle: {
+        fontSize: 21,
+        fontWeight: 'bold',
+        marginLeft: 15,
+        marginBottom: 5
+    },
+    contents: {
+        fontSize: 18,
+        marginLeft: 15,
+        marginBottom: 5
+    },
     imageStyle: {
         width: 250,
         height: 120,
         borderRadius: 4,
-        marginBottom: 5
+        marginBottom: 5,
+        marginLeft: 15,
     },
 });
 
